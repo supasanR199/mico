86bd9a1ff0c03a7f6b481f456fc687c4e0995c1e
supasan <supasan.garry@gmail.com>
2021-01-21 15:44:28 +0700

    lab5.2

--- lab5.2/lab5.2.ino
+++ lab5.2/lab5.2.ino
@@ -1,9 +1,31 @@
+int pwm_wave  = 5;
+int aVal;
+int unsigned long  presstime,previostime;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(pwm_wave,OUTPUT);
+  pinMode(13,INPUT);
+  pinMode(12,INPUT);
 
 }
-
 void loop() {
   // put your main code here, to run repeatedly:
+  if(digitalRead(13)==1){
+    while(digitalRead(13)==1){
+        aVal  = (aVal ==  1023) ? aVal  = 1023  : ++aVal;
+        Serial.println(aVal/4,DEC);
+        analogWrite(pwm_wave,aVal/4);
+      }
+    delay(200);
+    }
+   if(digitalRead(12)==1){
+    while(digitalRead(12)==1){
+        aVal  = (aVal ==  0) ? aVal  = 0  : --aVal;
+        Serial.println(aVal/4,DEC);
+        analogWrite(pwm_wave,aVal/4);
+      }
+    delay(200);
+    }
 
 }
