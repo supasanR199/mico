0ef7bb7b4d2182d5ce4bad45080f8b29feff6d37
supasan <supasan.garry@gmail.com>
2021-01-21 15:22:41 +0700

    lab5.1

--- lab5/lab5.ino
+++ lab5/lab5.ino
@@ -1,37 +1,41 @@
+int unsigned long presstime,previoustime,releasetime;
+int count=1,pointer = -1;
+int led[] = {11,10,9,8,7,6,5,4,3,2};
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   for(int pin=2;pin<=12;pin++){
     pinMode(pin,OUTPUT);
     }
+  pinMode(13,INPUT);
+  pinMode(12,INPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int aVal  = analogRead(A0);
-  Serial.println(aVal,DEC);
-  Serial.println(aVal/102,DEC);
-  delay(100);
-  aVal  = aVal/102;
-    switch(aVal){
-      case 1:aVal = 2;break;
-      case 2:aVal = 3;break;
-      case 3:aVal = 4;break;
-      case 4:aVal = 5;break;
-      case 5:aVal = 6;break;
-      case 6:aVal = 7;break;
-      case 7:aVal = 8;break;
-      case 8:aVal = 9;break;
-      case 9:aVal = 10;break;
-      case 10:aVal = 11;break;
-      default : aVal =0;
-      
-    }
-    for(int pin=2;pin<=12;pin++){
-      if(pin  ==  aVal)
-        digitalWrite(pin,HIGH);
-      else
-        digitalWrite(pin,LOW);
-        }
-
+  releasetime = 0;
+  if(digitalRead(13)==1){
+    presstime = millis();
+    Serial.println(pointer);
+    pointer = (pointer  ==  9)  ? pointer = 9 : ++pointer;  
+    digitalWrite(led[pointer-1],0);
+    while(digitalRead(13)==1){
+        digitalWrite(led[pointer],1);
+      }
+    delay(200); 
+  }
+   if(digitalRead(12)==1){
+    presstime = millis();
+    Serial.println(pointer);
+    if(pointer  ==  0)
+      pointer = 0;
+     else
+      --pointer;
+    digitalWrite(led[pointer+1],0);
+    while(digitalRead(12)==1){
+        digitalWrite(led[pointer],1);
+      }
+    delay(200); 
+  }
 }
